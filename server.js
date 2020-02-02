35c6a5b738bcee26fe6d45b643df27a76be421f8
Kerry Wang <kwzeyunwang@gmail.com>
2020-02-01 20:48:01 -0500

    honestly i dont even know whats happening anymore

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ var express = require('express')
 var app = express()
 
 app.use(express.static(__dirname))
+
+app.get('/', function(req, res){
+    res.redirect('/register.html');
+});
+
 var server = app.listen(8080, ()=>{
     console.log('server is listening on port', server.address().port)
 })
